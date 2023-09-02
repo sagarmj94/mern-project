f439a1d72dc09eb1c8fb774f0976cb07025d1f5b
sagar <sagarmj1502@gmail.com>
2023-09-02 16:02:31 +0530

    written a first code for nodejs

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+const userName = "sagar";
+console.log("userName", userName);
+
+fs.writeFile("user-data.txt", "Name: " + userName, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("WROTE FILE");
+});
