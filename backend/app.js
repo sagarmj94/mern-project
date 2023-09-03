7fff3fa51b876826a519010e21506f8efd1b7759
sagar <sagarmj1502@gmail.com>
2023-09-03 11:32:28 +0530

    node js through created server

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,23 @@
-const fs = require("fs");
-
-const userName = "sagar";
-console.log("userName", userName);
-
-fs.writeFile("user-data.txt", "Name: " + userName, (err) => {
-  if (err) {
-    console.log(err);
+const http = require("http");
+const server = http.createServer((req, res) => {
+  console.log("ICOMING REQUESt");
+  console.log(req.method, req.url);
+  if (req.method === "POST") {
+    let body = "";
+    req.on("end", () => {
+      const userName = body.split("=")[1];
+      res.end("<h1>" + userName + "</h1>");
+    });
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+  } else {
+    //   res.setHeader("Content-Type", "text/plain");
+    res.setHeader("Content-Type", "text/html");
+    res.end(
+      '<form method="POST"><input type="text" name="username"><button type="submit">Create user</button></form>'
+    );
   }
-  //   console.log("WROTE FILE");
 });
+
+server.listen(5000);
