c238f6c9b9eb51b7396f6b4b332d253758fd4957
sagar <sagarmj1502@gmail.com>
2023-09-03 16:09:01 +0530

    added nodemon

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,29 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  console.log("ICOMING REQUESt");
-  console.log(req.method, req.url);
-  if (req.method === "POST") {
-    let body = "";
-    req.on("end", () => {
-      const userName = body.split("=")[1];
-      res.end("<h1>" + userName + "</h1>");
-    });
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-  } else {
-    //   res.setHeader("Content-Type", "text/plain");
-    res.setHeader("Content-Type", "text/html");
-    res.end(
-      '<form method="POST"><input type="text" name="username"><button type="submit">Create user</button></form>'
-    );
-  }
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  let body = "";
+  req.on("end", () => {
+    const userName = body.split("=")[1];
+    if (userName) {
+      req.body = { name: userName };
+    }
+    next();
+  });
+  req.on("data", (chunk) => {
+    console.log("chnuk", chunk);
+    body += chunk;
+  });
 });
 
-server.listen(5000);
+app.use((req, res, next) => {
+  if (req.body) {
+    return res.send("<h1>" + req.body.name + "</h1>");
+  }
+  res.send(
+    '<form method="POST"><input type="text" name="username"><button type="submit">Create</button></form>'
+  );
+});
+
+app.listen(5000);
